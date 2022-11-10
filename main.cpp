b545ee1c200e4ed1a0b50731aef92e12c918f443
KlawyFarfocel <smierciak.maciej@gmail.com>
2022-11-10 17:05:55 +0100

    Version 1.4

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 void liniowa(){
-    int a,b;
+    float a,b;
     cout<<"Podaj a: ";
     cin>>a;
 
     cout<<"Podaj b: ";
     cin>>b;
-    int wynik;
+    float wynik;
     if(a==0){
         if(b==0){
             cout<<"Jest nieskonczenie wiele miejsc zerowych"<<endl;
@@ -21,11 +21,40 @@ void liniowa(){
     cout<<"Miejsce zerowe znajduje sie w x="<<wynik<<endl;
     return;
 }
+void kanoniczna(){
+    float a,b,c;
+    cout<<"Podaj a: ";
+    cin>>a;
 
+    cout<<"Podaj b: ";
+    cin>>b;
+
+    cout<<"Podaj c: ";
+    cin>>c;
+
+    if(a==0){
+        if(c==0){
+            cout<<"Jest nieskonczenie wiele miejsc zerowych"<<endl;
+            return;
+        }
+        cout<<"Nie ma miejsc zerowych"<<endl;
+        return;
+    }
+   else if (b==0 && c==0){
+        cout<<"Jest nieskonczenie wiele miejsc zerowych"<<endl;
+        return;
+    }
+    float nA,nC; //nowe a, nowe b, nowe c
+    nA=(-a/b);
+    nC=(-c/b);
+    float wynik=-nC/nA;
+    cout<<"Miejsce zerowe znajduje sie w x="<<wynik<<endl;
+}
 void menu(){
     system("cls");
     cout<<"Sieam, tu menu"<<endl;
     cout<<"---------------------1.Funkcja linowa---------------------"<<endl;
+    cout<<"---------------------2.Funkcja linowa, ale kanoniczna---------------------"<<endl;
     cout<<"---------------------0.Wyjscie---------------------"<<endl;
     cout<<"Wybierz opcje:";
 
@@ -35,11 +64,14 @@ void menu(){
     switch (wybor)
     {
     case 1:
-    liniowa();
-    system("pause");
-    menu();
-        break;
-    
+        liniowa();
+        system("pause");
+        menu();
+    break;
+    case 2:
+        kanoniczna();
+        system("pause");
+    break;
     default:
         break;
     }
