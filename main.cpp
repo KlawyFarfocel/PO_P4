b2d9e5b8b21901412b86e6190dbec519d9de6b9f
KlawyFarfocel <smierciak.maciej@gmail.com>
2022-11-10 13:13:46 +0100

    Version 1.1

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int liniowa(){
+void liniowa(){
     int a,b;
     cout<<"Podaj a: ";
     cin>>a;
-    if(a==0){
-        cout<<"A nie moze byc 0!"<<endl;
-        cin.clear();
-        cout<<"Podaj a: ";
-        cin>>a;
-    }
+
     cout<<"Podaj b: ";
     cin>>b;
     int wynik;
+    if(a==0){
+        if(b==0){
+            cout<<"Jest nieskonczenie wiele miejsc zerowych"<<endl;
+            return;
+        }
+         cout<<"Nie ma miejsc zerowych"<<endl;
+         return;
+    }
     wynik=(-b)/a; 
-    return wynik;
+    cout<<"Miejsce zerowe znajduje sie w x="<<wynik<<endl;
+    return;
 }
 
 void menu(){
@@ -24,15 +28,14 @@ void menu(){
     cout<<"---------------------1.Funkcja linowa---------------------"<<endl;
     cout<<"---------------------0.Wyjscie---------------------"<<endl;
     cout<<"Wybierz opcje:";
+
     int wybor;
     cin>>wybor;
     cout<<endl;
     switch (wybor)
     {
     case 1:
-    int wynik;
-    wynik=liniowa();
-    cout<<"Miejsce zerowe znajduje sie w x="<<wynik<<endl;
+    liniowa();
     system("pause");
     menu();
         break;
